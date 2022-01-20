05943ba8e3db9b2e0f71c6341588c70367b23359
ohaq13 <ohaq13@users.noreply.github.com>
2022-01-20 14:17:14 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://6c9192df.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
